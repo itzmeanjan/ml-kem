8b37b232fcf94e882371c0ac5e3d8a83e9af1a52
Anjan Roy <hello@itzmeanjan.in>
2022-09-20 17:34:59 +0400

    test polynomial (de)serialization for all possible values of l ∈ [1, 12]
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -11,5 +11,19 @@ main()
   std::cout << "[test] (i)NTT over degree-255 polynomial R_q | q = 3329"
             << std::endl;
 
+  test_kyber::test_serialization<12>();
+  test_kyber::test_serialization<11>();
+  test_kyber::test_serialization<10>();
+  test_kyber::test_serialization<9>();
+  test_kyber::test_serialization<8>();
+  test_kyber::test_serialization<7>();
+  test_kyber::test_serialization<6>();
+  test_kyber::test_serialization<5>();
+  test_kyber::test_serialization<4>();
+  test_kyber::test_serialization<3>();
+  test_kyber::test_serialization<2>();
+  test_kyber::test_serialization<1>();
+  std::cout << "[test] Polynomial serialization/ deserialization" << std::endl;
+
   return EXIT_SUCCESS;
 }

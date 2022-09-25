d623431fb47716ea7c753672eee957509a34843c
Anjan Roy <hello@itzmeanjan.in>
2022-09-25 09:57:53 +0400

    benchmark subtraction over Z_q | q = 3329
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -3,6 +3,7 @@
 // register for benchmarking arithmetic over prime field
 BENCHMARK(bench_kyber::ff_add);
 BENCHMARK(bench_kyber::ff_compound_add);
+BENCHMARK(bench_kyber::ff_sub);
 
 // benchmark runner main routine
 BENCHMARK_MAIN();
--- include/bench_ff.hpp
+++ include/bench_ff.hpp
@@ -49,4 +49,26 @@ ff_compound_add(benchmark::State& state)
   assert(c == b);
 }
 
+// Benchmark subtraction over prime field Z_q | q = 3329
+void
+ff_sub(benchmark::State& state)
+{
+  ff::ff_t a = ff::ff_t::random();
+  ff::ff_t b{};
+
+  for (auto _ : state) {
+    b = b - a;
+    benchmark::DoNotOptimize(b);
+  }
+
+  const size_t itr = state.iterations();
+
+  ff::ff_t c{};
+  for (size_t i = 0; i < itr; i++) {
+    c = c - a;
+  }
+
+  assert(c == b);
+}
+
 }

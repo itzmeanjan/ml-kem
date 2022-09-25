cdfb24296254ff477130b9bf6c25558c8842a64d
Anjan Roy <hello@itzmeanjan.in>
2022-09-25 09:49:24 +0400

    benchmark compound addition operator over Z_q
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -2,6 +2,7 @@
 
 // register for benchmarking arithmetic over prime field
 BENCHMARK(bench_kyber::ff_add);
+BENCHMARK(bench_kyber::ff_compound_add);
 
 // benchmark runner main routine
 BENCHMARK_MAIN();
--- include/bench_ff.hpp
+++ include/bench_ff.hpp
@@ -27,4 +27,26 @@ ff_add(benchmark::State& state)
   assert(c == b);
 }
 
+// Benchmark compound addition over prime field Z_q | q = 3329
+void
+ff_compound_add(benchmark::State& state)
+{
+  ff::ff_t a = ff::ff_t::random();
+  ff::ff_t b{};
+
+  for (auto _ : state) {
+    b += a;
+    benchmark::DoNotOptimize(b);
+  }
+
+  const size_t itr = state.iterations();
+
+  ff::ff_t c{};
+  for (size_t i = 0; i < itr; i++) {
+    c += a;
+  }
+
+  assert(c == b);
+}
+
 }

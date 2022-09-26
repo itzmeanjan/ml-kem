e9edf89c35b54d96a3ff551f783d85076810fcfb
Anjan Roy <hello@itzmeanjan.in>
2022-09-26 14:06:51 +0400

    benchmark IND-CCA2-secure Kyber KEM decapsulation algorithm
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -57,12 +57,15 @@ BENCHMARK(bench_kyber::decrypt<4, 2, 2, 11, 5>); // kyber1024
 // register for benchmarking IND-CCA2-secure Kyber Key Encapsulation Mechanism
 BENCHMARK(bench_kyber::kem_keygen<2, 3>);            // kyber512
 BENCHMARK(bench_kyber::encapsulate<2, 3, 2, 10, 4>); // kyber512
+BENCHMARK(bench_kyber::decapsulate<2, 3, 2, 10, 4>); // kyber512
 
 BENCHMARK(bench_kyber::kem_keygen<3, 2>);            // kyber768
 BENCHMARK(bench_kyber::encapsulate<3, 2, 2, 10, 4>); // kyber768
+BENCHMARK(bench_kyber::decapsulate<3, 2, 2, 10, 4>); // kyber768
 
 BENCHMARK(bench_kyber::kem_keygen<4, 2>);            // kyber1024
 BENCHMARK(bench_kyber::encapsulate<4, 2, 2, 11, 5>); // kyber1024
+BENCHMARK(bench_kyber::decapsulate<4, 2, 2, 11, 5>); // kyber1024
 
 // benchmark runner main routine
 BENCHMARK_MAIN();
--- include/bench_kem_kyber.hpp
+++ include/bench_kem_kyber.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "decapsulation.hpp"
 #include "encapsulation.hpp"
 #include "kem_keygen.hpp"
 #include <benchmark/benchmark.h>
@@ -76,6 +77,59 @@ encapsulate(benchmark::State& state)
   std::free(skey);
   std::free(cipher);
   std::free(sender_key);
+}
+
+// Benchmarking IND-CCA2-secure Kyber KEM decapsulation algorithm
+template<const size_t k,
+         const size_t eta1,
+         const size_t eta2,
+         const size_t du,
+         const size_t dv>
+void
+decapsulate(benchmark::State& state)
+{
+  constexpr size_t pklen = k * 12 * 32 + 32;
+  constexpr size_t sklen = k * 12 * 32 + pklen + 32 + 32;
+  constexpr size_t ctlen = k * du * 32 + dv * 32;
+  constexpr size_t klen = 32;
+
+  uint8_t* pkey = static_cast<uint8_t*>(std::malloc(pklen));
+  uint8_t* skey = static_cast<uint8_t*>(std::malloc(sklen));
+  uint8_t* cipher = static_cast<uint8_t*>(std::malloc(ctlen));
+  uint8_t* sender_key = static_cast<uint8_t*>(std::malloc(klen));
+  uint8_t* receiver_key = static_cast<uint8_t*>(std::malloc(klen));
+
+  std::memset(pkey, 0, pklen);
+  std::memset(skey, 0, sklen);
+  std::memset(cipher, 0, ctlen);
+  std::memset(sender_key, 0, klen);
+  std::memset(receiver_key, 0, klen);
+
+  ccakem::keygen<k, eta1>(pkey, skey);
+  auto skdf = ccakem::encapsulate<k, eta1, eta2, du, dv>(pkey, cipher);
+  skdf.read(sender_key, klen);
+
+  for (auto _ : state) {
+    auto rkdf = ccakem::decapsulate<k, eta1, eta2, du, dv>(skey, cipher);
+    rkdf.read(receiver_key, klen);
+
+    benchmark::DoNotOptimize(skey);
+    benchmark::DoNotOptimize(cipher);
+    benchmark::DoNotOptimize(receiver_key);
+    benchmark::ClobberMemory();
+  }
+
+  for (size_t i = 0; i < klen; i++) {
+    assert(!static_cast<bool>(sender_key[i] ^ receiver_key[i]));
+  }
+
+  state.SetItemsProcessed(static_cast<int64_t>(state.iterations()));
+
+  std::free(pkey);
+  std::free(skey);
+  std::free(cipher);
+  std::free(sender_key);
+  std::free(receiver_key);
 }
 
 }

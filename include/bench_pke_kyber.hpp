b8bd3229fa2f1d154b4257b36793df61c20af7cb
Anjan Roy <hello@itzmeanjan.in>
2022-09-26 12:53:34 +0400

    benchmark IND-CPA-secure Kyber PKE message ( of 32 -bytes ) encryption algorithm
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/bench_pke_kyber.hpp
+++ include/bench_pke_kyber.hpp
@@ -1,12 +1,12 @@
 #pragma once
+#include "encryption.hpp"
 #include "pke_keygen.hpp"
 #include <benchmark/benchmark.h>
 
 // Benchmark Kyber PQC suite implementation on CPU, using google-benchmark
 namespace bench_kyber {
 
-// Benchmarking IND-CPA-secure Kyber Public Key Encryption  ( PKE ) key
-// generation algorithm
+// Benchmarking IND-CPA-secure Kyber PKE key generation algorithm
 template<const size_t k, const size_t eta1>
 void
 keygen(benchmark::State& state)
@@ -32,6 +32,54 @@ keygen(benchmark::State& state)
 
   std::free(pkey);
   std::free(skey);
+}
+
+// Benchmarking IND-CPA-secure Kyber public key encryption algorithm
+template<const size_t k,
+         const size_t eta1,
+         const size_t eta2,
+         const size_t du,
+         const size_t dv>
+void
+encrypt(benchmark::State& state)
+{
+  constexpr size_t pklen = k * 12 * 32 + 32;
+  constexpr size_t sklen = k * 12 * 32;
+  constexpr size_t mlen = 32;
+  constexpr size_t enclen = k * du * 32 + dv * 32;
+
+  uint8_t* pkey = static_cast<uint8_t*>(std::malloc(pklen));
+  uint8_t* skey = static_cast<uint8_t*>(std::malloc(sklen));
+  uint8_t* rcoin = static_cast<uint8_t*>(std::malloc(mlen));
+  uint8_t* txt = static_cast<uint8_t*>(std::malloc(mlen));
+  uint8_t* enc = static_cast<uint8_t*>(std::malloc(enclen));
+
+  std::memset(pkey, 0, pklen);
+  std::memset(skey, 0, sklen);
+  std::memset(enc, 0, enclen);
+
+  random_data<uint8_t>(txt, mlen);
+  random_data<uint8_t>(rcoin, mlen);
+
+  cpapke::keygen<k, eta1>(pkey, skey);
+
+  for (auto _ : state) {
+    cpapke::encrypt<k, eta1, eta2, du, dv>(pkey, txt, rcoin, enc);
+
+    benchmark::DoNotOptimize(pkey);
+    benchmark::DoNotOptimize(txt);
+    benchmark::DoNotOptimize(rcoin);
+    benchmark::DoNotOptimize(enc);
+    benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(static_cast<int64_t>(state.iterations()));
+
+  std::free(pkey);
+  std::free(skey);
+  std::free(rcoin);
+  std::free(txt);
+  std::free(enc);
 }
 
 }

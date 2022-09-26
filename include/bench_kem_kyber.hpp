d446e9dfda01e590d771d798150c2d0dbc10d04d
Anjan Roy <hello@itzmeanjan.in>
2022-09-26 13:59:17 +0400

    benchmark key encapsulation algorithm of IND-CCA2-secure Kyber KEM
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/bench_kem_kyber.hpp
+++ include/bench_kem_kyber.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "encapsulation.hpp"
 #include "kem_keygen.hpp"
 #include <benchmark/benchmark.h>
 
@@ -31,6 +32,50 @@ kem_keygen(benchmark::State& state)
 
   std::free(pkey);
   std::free(skey);
+}
+
+// Benchmarking IND-CCA2-secure Kyber KEM encapsulation algorithm
+template<const size_t k,
+         const size_t eta1,
+         const size_t eta2,
+         const size_t du,
+         const size_t dv>
+void
+encapsulate(benchmark::State& state)
+{
+  constexpr size_t pklen = k * 12 * 32 + 32;
+  constexpr size_t sklen = k * 12 * 32 + pklen + 32 + 32;
+  constexpr size_t ctlen = k * du * 32 + dv * 32;
+  constexpr size_t klen = 32;
+
+  uint8_t* pkey = static_cast<uint8_t*>(std::malloc(pklen));
+  uint8_t* skey = static_cast<uint8_t*>(std::malloc(sklen));
+  uint8_t* cipher = static_cast<uint8_t*>(std::malloc(ctlen));
+  uint8_t* sender_key = static_cast<uint8_t*>(std::malloc(klen));
+
+  std::memset(pkey, 0, pklen);
+  std::memset(skey, 0, sklen);
+  std::memset(cipher, 0, ctlen);
+  std::memset(sender_key, 0, klen);
+
+  ccakem::keygen<k, eta1>(pkey, skey);
+
+  for (auto _ : state) {
+    auto skdf = ccakem::encapsulate<k, eta1, eta2, du, dv>(pkey, cipher);
+    skdf.read(sender_key, klen);
+
+    benchmark::DoNotOptimize(pkey);
+    benchmark::DoNotOptimize(cipher);
+    benchmark::DoNotOptimize(sender_key);
+    benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(static_cast<int64_t>(state.iterations()));
+
+  std::free(pkey);
+  std::free(skey);
+  std::free(cipher);
+  std::free(sender_key);
 }
 
 }

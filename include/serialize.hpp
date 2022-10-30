93db704a2b5a1501747409917b91c6a86c7970cf
Anjan Roy <hello@itzmeanjan.in>
2022-10-30 12:35:50 +0400

    specialized byte (de)serialization implementation when 1 -bit of each coefficient (from LSB side) is considered to be of importance
    LSB -> Least Significant Bit
    
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/serialize.hpp
+++ include/serialize.hpp
@@ -39,15 +39,34 @@ encode(const ff::ff_t* const __restrict poly, // degree 255 polynomial
 
   std::memset(arr, 0, len);
 
-  for (size_t i = 0; i < blen; i++) {
-    const size_t pidx = i / l;
-    const size_t poff = i % l;
+  if constexpr (l == 1) {
+    constexpr size_t itr_cnt = ntt::N >> 3;
+    constexpr uint16_t one = 0b1;
 
-    const size_t aidx = i >> 3;
-    const size_t aoff = i & 7ul;
+    for (size_t i = 0; i < itr_cnt; i++) {
+      const size_t off = i << 3;
+      const uint8_t byte = (static_cast<uint8_t>(poly[off ^ 7].v & one) << 7) |
+                           (static_cast<uint8_t>(poly[off ^ 6].v & one) << 6) |
+                           (static_cast<uint8_t>(poly[off ^ 5].v & one) << 5) |
+                           (static_cast<uint8_t>(poly[off ^ 4].v & one) << 4) |
+                           (static_cast<uint8_t>(poly[off ^ 3].v & one) << 3) |
+                           (static_cast<uint8_t>(poly[off ^ 2].v & one) << 2) |
+                           (static_cast<uint8_t>(poly[off ^ 1].v & one) << 1) |
+                           (static_cast<uint8_t>(poly[off ^ 0].v & one) << 0);
 
-    const uint8_t bit = static_cast<uint8_t>((poly[pidx].v >> poff) & 0b1);
-    arr[aidx] = arr[aidx] ^ (bit << aoff);
+      arr[i] = byte;
+    }
+  } else {
+    for (size_t i = 0; i < blen; i++) {
+      const size_t pidx = i / l;
+      const size_t poff = i % l;
+
+      const size_t aidx = i >> 3;
+      const size_t aoff = i & 7ul;
+
+      const uint8_t bit = static_cast<uint8_t>((poly[pidx].v >> poff) & 0b1);
+      arr[aidx] = arr[aidx] ^ (bit << aoff);
+    }
   }
 }
 
@@ -70,15 +89,34 @@ decode(const uint8_t* const __restrict arr, // byte array of length 32*l -bytes
 
   std::memset(poly, 0, n * sizeof(ff::ff_t));
 
-  for (size_t i = 0; i < blen; i++) {
-    const size_t aidx = i >> 3;
-    const size_t aoff = i & 7ul;
+  if constexpr (l == 1) {
+    constexpr size_t itr_cnt = ntt::N >> 3;
+    constexpr uint16_t one = 0b1;
 
-    const size_t pidx = i / l;
-    const size_t poff = i % l;
+    for (size_t i = 0; i < itr_cnt; i++) {
+      const size_t off = i << 3;
+      const uint8_t byte = arr[i];
 
-    const uint8_t bit = (arr[aidx] >> aoff) & 0b1;
-    poly[pidx].v = poly[pidx].v ^ static_cast<uint16_t>(bit) << poff;
+      poly[off ^ 0].v = (byte >> 0) & one;
+      poly[off ^ 1].v = (byte >> 1) & one;
+      poly[off ^ 2].v = (byte >> 2) & one;
+      poly[off ^ 3].v = (byte >> 3) & one;
+      poly[off ^ 4].v = (byte >> 4) & one;
+      poly[off ^ 5].v = (byte >> 5) & one;
+      poly[off ^ 6].v = (byte >> 6) & one;
+      poly[off ^ 7].v = (byte >> 7) & one;
+    }
+  } else {
+    for (size_t i = 0; i < blen; i++) {
+      const size_t aidx = i >> 3;
+      const size_t aoff = i & 7ul;
+
+      const size_t pidx = i / l;
+      const size_t poff = i % l;
+
+      const uint8_t bit = (arr[aidx] >> aoff) & 0b1;
+      poly[pidx].v = poly[pidx].v ^ static_cast<uint16_t>(bit) << poff;
+    }
   }
 }
 

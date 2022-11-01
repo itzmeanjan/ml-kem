ea1a732454aecb8fd8ddc515ff280d425b5aaa23
Anjan Roy <hello@itzmeanjan.in>
2022-11-01 11:48:56 +0400

    updated Kyber KEM interface so that it's actually easy to test using KATs
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- wrapper/kyber_kem.cpp
+++ wrapper/kyber_kem.cpp
@@ -1,20 +1,32 @@
-#include "kyber_kem.hpp"
+#include "decapsulation.hpp"
+#include "encapsulation.hpp"
+#include "kem_keygen.hpp"
 
 // Thin C wrapper on top of underlying C++ implementation of Kyber{512, 768,
 // 1024} KEM, which can be used for producing shared library object with
 // conformant C-ABI & interfaced from other languages such as Rust, Python etc.
+//
+// This interface is being exposed so that it becomes easier to test this
+// implementation's functional correctness and conformance with Kyber
+// specification
+// https://pq-crystals.org/kyber/data/kyber-specification-round3-20210804.pdf
+// using known answer tests produced using
+// https://gist.github.com/itzmeanjan/c8f5bc9640d0f0bdd2437dfe364d7710
 
 // Function prototypes
 extern "C"
 {
 
   // Kyber512 KEM keypair generation
-  void keygen512(uint8_t* const __restrict pubkey,
+  void keygen512(const uint8_t* const __restrict d,
+                 const uint8_t* const __restrict z,
+                 uint8_t* const __restrict pubkey,
                  uint8_t* const __restrict seckey);
 
   // Kyber512 KEM encapsulation, with shared secret key ( of length klen bytes )
   // derivation
-  void encapsulate512(const uint8_t* const __restrict pubkey,
+  void encapsulate512(const uint8_t* const __restrict m,
+                      const uint8_t* const __restrict pubkey,
                       uint8_t* const __restrict cipher,
                       uint8_t* const __restrict shared_key,
                       const size_t klen);
@@ -27,12 +39,15 @@ extern "C"
                       const size_t klen);
 
   // Kyber768 KEM keypair generation
-  void keygen768(uint8_t* const __restrict pubkey,
+  void keygen768(const uint8_t* const __restrict d,
+                 const uint8_t* const __restrict z,
+                 uint8_t* const __restrict pubkey,
                  uint8_t* const __restrict seckey);
 
   // Kyber768 KEM encapsulation, with shared secret key ( of length klen bytes )
   // derivation
-  void encapsulate768(const uint8_t* const __restrict pubkey,
+  void encapsulate768(const uint8_t* const __restrict m,
+                      const uint8_t* const __restrict pubkey,
                       uint8_t* const __restrict cipher,
                       uint8_t* const __restrict shared_key,
                       const size_t klen);
@@ -45,12 +60,15 @@ extern "C"
                       const size_t klen);
 
   // Kyber1024 KEM keypair generation
-  void keygen1024(uint8_t* const __restrict pubkey,
+  void keygen1024(const uint8_t* const __restrict d,
+                  const uint8_t* const __restrict z,
+                  uint8_t* const __restrict pubkey,
                   uint8_t* const __restrict seckey);
 
   // Kyber1024 KEM encapsulation, with shared secret key ( of length klen bytes
   // ) derivation
-  void encapsulate1024(const uint8_t* const __restrict pubkey,
+  void encapsulate1024(const uint8_t* const __restrict m,
+                       const uint8_t* const __restrict pubkey,
                        uint8_t* const __restrict cipher,
                        uint8_t* const __restrict shared_key,
                        const size_t klen);
@@ -67,18 +85,21 @@ extern "C"
 extern "C"
 {
 
-  void keygen512(uint8_t* const __restrict pubkey,
+  void keygen512(const uint8_t* const __restrict d,
+                 const uint8_t* const __restrict z,
+                 uint8_t* const __restrict pubkey,
                  uint8_t* const __restrict seckey)
   {
-    kyber_kem::keygen<2, 3>(pubkey, seckey);
+    ccakem::keygen<2, 3>(d, z, pubkey, seckey);
   }
 
-  void encapsulate512(const uint8_t* const __restrict pubkey,
+  void encapsulate512(const uint8_t* const __restrict m,
+                      const uint8_t* const __restrict pubkey,
                       uint8_t* const __restrict cipher,
                       uint8_t* const __restrict shared_key,
                       const size_t klen)
   {
-    auto kdf = kyber_kem::encapsulate<2, 3, 2, 10, 4>(pubkey, cipher);
+    auto kdf = ccakem::encapsulate<2, 3, 2, 10, 4>(m, pubkey, cipher);
     kdf.read(shared_key, klen);
   }
 
@@ -87,22 +108,25 @@ extern "C"
                       uint8_t* const __restrict shared_key,
                       const size_t klen)
   {
-    auto kdf = kyber_kem::decapsulate<2, 3, 2, 10, 4>(seckey, cipher);
+    auto kdf = ccakem::decapsulate<2, 3, 2, 10, 4>(seckey, cipher);
     kdf.read(shared_key, klen);
   }
 
-  void keygen768(uint8_t* const __restrict pubkey,
+  void keygen768(const uint8_t* const __restrict d,
+                 const uint8_t* const __restrict z,
+                 uint8_t* const __restrict pubkey,
                  uint8_t* const __restrict seckey)
   {
-    kyber_kem::keygen<3, 2>(pubkey, seckey);
+    ccakem::keygen<3, 2>(d, z, pubkey, seckey);
   }
 
-  void encapsulate768(const uint8_t* const __restrict pubkey,
+  void encapsulate768(const uint8_t* const __restrict m,
+                      const uint8_t* const __restrict pubkey,
                       uint8_t* const __restrict cipher,
                       uint8_t* const __restrict shared_key,
                       const size_t klen)
   {
-    auto kdf = kyber_kem::encapsulate<3, 2, 2, 10, 4>(pubkey, cipher);
+    auto kdf = ccakem::encapsulate<3, 2, 2, 10, 4>(m, pubkey, cipher);
     kdf.read(shared_key, klen);
   }
 
@@ -111,22 +135,25 @@ extern "C"
                       uint8_t* const __restrict shared_key,
                       const size_t klen)
   {
-    auto kdf = kyber_kem::decapsulate<3, 2, 2, 10, 4>(seckey, cipher);
+    auto kdf = ccakem::decapsulate<3, 2, 2, 10, 4>(seckey, cipher);
     kdf.read(shared_key, klen);
   }
 
-  void keygen1024(uint8_t* const __restrict pubkey,
+  void keygen1024(const uint8_t* const __restrict d,
+                  const uint8_t* const __restrict z,
+                  uint8_t* const __restrict pubkey,
                   uint8_t* const __restrict seckey)
   {
-    kyber_kem::keygen<4, 2>(pubkey, seckey);
+    ccakem::keygen<4, 2>(d, z, pubkey, seckey);
   }
 
-  void encapsulate1024(const uint8_t* const __restrict pubkey,
+  void encapsulate1024(const uint8_t* const __restrict m,
+                       const uint8_t* const __restrict pubkey,
                        uint8_t* const __restrict cipher,
                        uint8_t* const __restrict shared_key,
                        const size_t klen)
   {
-    auto kdf = kyber_kem::encapsulate<4, 2, 2, 11, 5>(pubkey, cipher);
+    auto kdf = ccakem::encapsulate<4, 2, 2, 11, 5>(m, pubkey, cipher);
     kdf.read(shared_key, klen);
   }
 
@@ -135,7 +162,7 @@ extern "C"
                        uint8_t* const __restrict shared_key,
                        const size_t klen)
   {
-    auto kdf = kyber_kem::decapsulate<4, 2, 2, 11, 5>(seckey, cipher);
+    auto kdf = ccakem::decapsulate<4, 2, 2, 11, 5>(seckey, cipher);
     kdf.read(shared_key, klen);
   }
 }

9ea049fb75769393fbf0b4a425e526cc84898f87
Anjan Roy <hello@itzmeanjan.in>
2023-02-28 18:16:08 +0400

    update kyber single import header file
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/kyber.hpp
+++ include/kyber.hpp
@@ -2,5 +2,10 @@
 
 // Kyber Public Key Encryption (PKE) and Key Encapsulation Mechanism (KEM)
 
-#include "kyber_kem.hpp"
-#include "kyber_pke.hpp"
+#include "kyber1024_kem.hpp"
+#include "kyber1024_pke.hpp"
+#include "kyber512_kem.hpp"
+#include "kyber512_pke.hpp"
+#include "kyber768_kem.hpp"
+#include "kyber768_pke.hpp"
+#include "utils.hpp"

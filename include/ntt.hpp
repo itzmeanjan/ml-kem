be7b613b0b97f1873adfff3ace0a4367e389f5e4
Anjan Roy <hello@itzmeanjan.in>
2023-03-09 11:49:19 +0400

    compile-time compute powers of `ζ` for (i)NTT
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/ntt.hpp
+++ include/ntt.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "field.hpp"
+#include <array>
 #include <cstring>
 
 // (inverse) Number Theoretic Transform for degree 255 polynomial, over Kyber
@@ -16,60 +17,6 @@ constexpr ff::ff_t ζ{ 17 };
 
 // Multiplicative inverse of N/ 2 over Z_q | q = 3329 and N = 256
 constexpr auto INV_N = ff::ff_t{ N >> 1 }.inv();
-
-// Precomputed constants ( powers of ζ ), used for computing NTT form of
-// degree-255 polynomial
-constexpr ff::ff_t NTT_ζ_EXP[]{
-  1,    1729, 2580, 3289, 2642, 630,  1897, 848,  1062, 1919, 193,  797,  2786,
-  3260, 569,  1746, 296,  2447, 1339, 1476, 3046, 56,   2240, 1333, 1426, 2094,
-  535,  2882, 2393, 2879, 1974, 821,  289,  331,  3253, 1756, 1197, 2304, 2277,
-  2055, 650,  1977, 2513, 632,  2865, 33,   1320, 1915, 2319, 1435, 807,  452,
-  1438, 2868, 1534, 2402, 2647, 2617, 1481, 648,  2474, 3110, 1227, 910,  17,
-  2761, 583,  2649, 1637, 723,  2288, 1100, 1409, 2662, 3281, 233,  756,  2156,
-  3015, 3050, 1703, 1651, 2789, 1789, 1847, 952,  1461, 2687, 939,  2308, 2437,
-  2388, 733,  2337, 268,  641,  1584, 2298, 2037, 3220, 375,  2549, 2090, 1645,
-  1063, 319,  2773, 757,  2099, 561,  2466, 2594, 2804, 1092, 403,  1026, 1143,
-  2150, 2775, 886,  1722, 1212, 1874, 1029, 2110, 2935, 885,  2154
-};
-
-// Precomputed constants ( negated powers of ζ ), used for computing coefficient
-// form of degree-255 polynomial using inverse NTT
-//
-// [-NTT_ζ_EXP[i] for i in range(128)]
-//
-// That's how these constants were computed !
-constexpr ff::ff_t INTT_ζ_EXP[]{
-  3328, 1600, 749,  40,   687,  2699, 1432, 2481, 2267, 1410, 3136, 2532, 543,
-  69,   2760, 1583, 3033, 882,  1990, 1853, 283,  3273, 1089, 1996, 1903, 1235,
-  2794, 447,  936,  450,  1355, 2508, 3040, 2998, 76,   1573, 2132, 1025, 1052,
-  1274, 2679, 1352, 816,  2697, 464,  3296, 2009, 1414, 1010, 1894, 2522, 2877,
-  1891, 461,  1795, 927,  682,  712,  1848, 2681, 855,  219,  2102, 2419, 3312,
-  568,  2746, 680,  1692, 2606, 1041, 2229, 1920, 667,  48,   3096, 2573, 1173,
-  314,  279,  1626, 1678, 540,  1540, 1482, 2377, 1868, 642,  2390, 1021, 892,
-  941,  2596, 992,  3061, 2688, 1745, 1031, 1292, 109,  2954, 780,  1239, 1684,
-  2266, 3010, 556,  2572, 1230, 2768, 863,  735,  525,  2237, 2926, 2303, 2186,
-  1179, 554,  2443, 1607, 2117, 1455, 2300, 1219, 394,  2444, 1175
-};
-
-// Precomputed constants ( powers of ζ ), used when multiplying two degree-255
-// polynomials in NTT domain
-//
-// [ ζ ^ ((br<7>(i) << 1) ^ 1) for i in range(128) ]
-//
-// s.t. ζ = 17 and br<7>(i) is bit reversal of `i`, while only considering lower
-// 7 -bits of `i`.
-constexpr ff::ff_t POLY_MUL_ζ_EXP[]{
-  17,   3312, 2761, 568,  583,  2746, 2649, 680,  1637, 1692, 723,  2606, 2288,
-  1041, 1100, 2229, 1409, 1920, 2662, 667,  3281, 48,   233,  3096, 756,  2573,
-  2156, 1173, 3015, 314,  3050, 279,  1703, 1626, 1651, 1678, 2789, 540,  1789,
-  1540, 1847, 1482, 952,  2377, 1461, 1868, 2687, 642,  939,  2390, 2308, 1021,
-  2437, 892,  2388, 941,  733,  2596, 2337, 992,  268,  3061, 641,  2688, 1584,
-  1745, 2298, 1031, 2037, 1292, 3220, 109,  375,  2954, 2549, 780,  2090, 1239,
-  1645, 1684, 1063, 2266, 319,  3010, 2773, 556,  757,  2572, 2099, 1230, 561,
-  2768, 2466, 863,  2594, 735,  2804, 525,  1092, 2237, 403,  2926, 1026, 2303,
-  1143, 2186, 2150, 1179, 2775, 554,  886,  2443, 1722, 1607, 1212, 2117, 1874,
-  1455, 1029, 2300, 2110, 1219, 2935, 394,  885,  2444, 2154, 1175
-};
 
 // Given a 64 -bit unsigned integer, this routine extracts specified many
 // contiguous bits from ( least significant bits ) LSB side & reverses their bit
@@ -91,6 +38,59 @@ bit_rev(const size_t v)
 
   return v_rev;
 }
+
+// Compile-time compute powers of ζ, used for polynomial evaluation ( NTT )
+consteval std::array<ff::ff_t, N / 2>
+compute_ntt_ζ()
+{
+  std::array<ff::ff_t, N / 2> res;
+
+  for (size_t i = 0; i < N / 2; i++) {
+    res[i] = ζ ^ bit_rev<LOG2N - 1>(i);
+  }
+
+  return res;
+}
+
+// Precomputed constants ( powers of ζ ), used for computing NTT form of
+// degree-255 polynomial
+constexpr std::array<ff::ff_t, N / 2> NTT_ζ_EXP = compute_ntt_ζ();
+
+// Compile-time compute negated powers of ζ, used for polynomial interpolation (
+// iNTT )
+consteval std::array<ff::ff_t, N / 2>
+compute_intt_ζ()
+{
+  std::array<ff::ff_t, N / 2> res;
+
+  for (size_t i = 0; i < N / 2; i++) {
+    res[i] = -NTT_ζ_EXP[i];
+  }
+
+  return res;
+}
+
+// Precomputed constants ( negated powers of ζ ), used for computing coefficient
+// form of degree-255 polynomial using inverse NTT
+constexpr std::array<ff::ff_t, N / 2> INTT_ζ_EXP = compute_intt_ζ();
+
+// Compile-time compute powers of ζ, used for multiplication of two degree-255
+// polynomials in NTT representation.
+consteval std::array<ff::ff_t, N / 2>
+compute_mul_ζ()
+{
+  std::array<ff::ff_t, N / 2> res;
+
+  for (size_t i = 0; i < N / 2; i++) {
+    res[i] = ζ ^ ((bit_rev<LOG2N - 1>(i) << 1) ^ 1);
+  }
+
+  return res;
+}
+
+// Precomputed constants ( powers of ζ ), used when multiplying two degree-255
+// polynomials in NTT domain.
+constexpr std::array<ff::ff_t, N / 2> POLY_MUL_ζ_EXP = compute_mul_ζ();
 
 // Given a polynomial f with 256 coefficients over F_q | q = 3329, this routine
 // computes number theoretic transform using cooley-tukey algorithm, producing

eec43d830b94f8bbee5544e8b926b895dadfd26f
Anjan Roy <hello@itzmeanjan.in>
2023-03-09 15:39:22 +0400

    update serialization of polynomial to bytearrays
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/serialize.hpp
+++ include/serialize.hpp
@@ -15,7 +15,7 @@ namespace kyber_utils {
 // https://pq-crystals.org/kyber/data/kyber-specification-round3-20210804.pdf
 template<const size_t l>
 static inline void
-encode(const ff::ff_t* const __restrict poly, // degree 255 polynomial
+encode(const field::zq_t* const __restrict poly, // degree 255 polynomial
        uint8_t* const __restrict arr // byte array of length 32*l -bytes
        )
   requires(kyber_params::check_l(l))
@@ -25,124 +25,146 @@ encode(const ff::ff_t* const __restrict poly, // degree 255 polynomial
 
   if constexpr (l == 1) {
     constexpr size_t itr_cnt = ntt::N >> 3;
-    constexpr uint16_t one = 0b1;
+    constexpr uint32_t one = 0b1u;
 
     for (size_t i = 0; i < itr_cnt; i++) {
       const size_t off = i << 3;
-      const uint8_t byte = (static_cast<uint8_t>(poly[off + 7].v & one) << 7) |
-                           (static_cast<uint8_t>(poly[off + 6].v & one) << 6) |
-                           (static_cast<uint8_t>(poly[off + 5].v & one) << 5) |
-                           (static_cast<uint8_t>(poly[off + 4].v & one) << 4) |
-                           (static_cast<uint8_t>(poly[off + 3].v & one) << 3) |
-                           (static_cast<uint8_t>(poly[off + 2].v & one) << 2) |
-                           (static_cast<uint8_t>(poly[off + 1].v & one) << 1) |
-                           (static_cast<uint8_t>(poly[off + 0].v & one) << 0);
-
-      arr[i] = byte;
+      arr[i] = (static_cast<uint8_t>(poly[off + 7].to_canonical() & one) << 7) |
+               (static_cast<uint8_t>(poly[off + 6].to_canonical() & one) << 6) |
+               (static_cast<uint8_t>(poly[off + 5].to_canonical() & one) << 5) |
+               (static_cast<uint8_t>(poly[off + 4].to_canonical() & one) << 4) |
+               (static_cast<uint8_t>(poly[off + 3].to_canonical() & one) << 3) |
+               (static_cast<uint8_t>(poly[off + 2].to_canonical() & one) << 2) |
+               (static_cast<uint8_t>(poly[off + 1].to_canonical() & one) << 1) |
+               (static_cast<uint8_t>(poly[off + 0].to_canonical() & one) << 0);
     }
   } else if constexpr (l == 4) {
     constexpr size_t itr_cnt = ntt::N >> 1;
-    constexpr uint16_t mask = 0b1111;
+    constexpr uint32_t msk = 0b1111u;
 
     for (size_t i = 0; i < itr_cnt; i++) {
       const size_t off = i << 1;
-      const uint8_t byte = (static_cast<uint8_t>(poly[off + 1].v & mask) << 4) |
-                           (static_cast<uint8_t>(poly[off + 0].v & mask) << 0);
-
-      arr[i] = byte;
+      arr[i] = (static_cast<uint8_t>(poly[off + 1].to_canonical() & msk) << 4) |
+               (static_cast<uint8_t>(poly[off + 0].to_canonical() & msk) << 0);
     }
   } else if constexpr (l == 5) {
     constexpr size_t itr_cnt = ntt::N >> 3;
-    constexpr uint16_t mask5 = 0b11111;
-    constexpr uint16_t mask4 = 0b1111;
-    constexpr uint16_t mask3 = 0b111;
-    constexpr uint16_t mask2 = 0b11;
-    constexpr uint16_t mask1 = 0b1;
+    constexpr uint32_t mask5 = 0b11111u;
+    constexpr uint32_t mask4 = 0b1111u;
+    constexpr uint32_t mask3 = 0b111u;
+    constexpr uint32_t mask2 = 0b11u;
+    constexpr uint32_t mask1 = 0b1u;
 
     for (size_t i = 0; i < itr_cnt; i++) {
       const size_t poff = i << 3;
       const size_t boff = i * 5;
 
-      arr[boff + 0] = (static_cast<uint8_t>(poly[poff + 1].v & mask3) << 5) |
-                      (static_cast<uint8_t>(poly[poff + 0].v & mask5) << 0);
-      arr[boff + 1] = (static_cast<uint8_t>(poly[poff + 3].v & mask1) << 7) |
-                      (static_cast<uint8_t>(poly[poff + 2].v & mask5) << 2) |
-                      static_cast<uint8_t>((poly[poff + 1].v >> 3) & mask2);
-      arr[boff + 2] = (static_cast<uint8_t>(poly[poff + 4].v & mask4) << 4) |
-                      static_cast<uint8_t>((poly[poff + 3].v >> 1) & mask4);
-      arr[boff + 3] = (static_cast<uint8_t>(poly[poff + 6].v & mask2) << 6) |
-                      (static_cast<uint8_t>(poly[poff + 5].v & mask5) << 1) |
-                      static_cast<uint8_t>((poly[poff + 4].v >> 4) & mask1);
-      arr[boff + 4] = (static_cast<uint8_t>(poly[poff + 7].v & mask5) << 3) |
-                      static_cast<uint8_t>((poly[poff + 6].v >> 2) & mask3);
+      const auto t0 = poly[poff + 0].to_canonical();
+      const auto t1 = poly[poff + 1].to_canonical();
+      const auto t2 = poly[poff + 2].to_canonical();
+      const auto t3 = poly[poff + 3].to_canonical();
+      const auto t4 = poly[poff + 4].to_canonical();
+      const auto t5 = poly[poff + 5].to_canonical();
+      const auto t6 = poly[poff + 6].to_canonical();
+      const auto t7 = poly[poff + 7].to_canonical();
+
+      arr[boff + 0] = (static_cast<uint8_t>(t1 & mask3) << 5) |
+                      (static_cast<uint8_t>(t0 & mask5) << 0);
+      arr[boff + 1] = (static_cast<uint8_t>(t3 & mask1) << 7) |
+                      (static_cast<uint8_t>(t2 & mask5) << 2) |
+                      static_cast<uint8_t>((t1 >> 3) & mask2);
+      arr[boff + 2] = (static_cast<uint8_t>(t4 & mask4) << 4) |
+                      static_cast<uint8_t>((t3 >> 1) & mask4);
+      arr[boff + 3] = (static_cast<uint8_t>(t6 & mask2) << 6) |
+                      (static_cast<uint8_t>(t5 & mask5) << 1) |
+                      static_cast<uint8_t>((t4 >> 4) & mask1);
+      arr[boff + 4] = (static_cast<uint8_t>(t7 & mask5) << 3) |
+                      static_cast<uint8_t>((t6 >> 2) & mask3);
     }
   } else if constexpr (l == 10) {
     constexpr size_t itr_cnt = ntt::N >> 2;
-    constexpr uint16_t mask6 = 0b111111;
-    constexpr uint16_t mask4 = 0b1111;
-    constexpr uint16_t mask2 = 0b11;
+    constexpr uint32_t mask6 = 0b111111u;
+    constexpr uint32_t mask4 = 0b1111u;
+    constexpr uint32_t mask2 = 0b11u;
 
     for (size_t i = 0; i < itr_cnt; i++) {
       const size_t poff = i << 2;
       const size_t boff = i * 5;
 
-      arr[boff + 0] = static_cast<uint8_t>(poly[poff + 0].v);
-      arr[boff + 1] = static_cast<uint8_t>((poly[poff + 1].v & mask6) << 2) |
-                      static_cast<uint8_t>((poly[poff + 0].v >> 8) & mask2);
-      arr[boff + 2] = static_cast<uint8_t>((poly[poff + 2].v & mask4) << 4) |
-                      static_cast<uint8_t>((poly[poff + 1].v >> 6) & mask4);
-      arr[boff + 3] = static_cast<uint8_t>((poly[poff + 3].v & mask2) << 6) |
-                      static_cast<uint8_t>((poly[poff + 2].v >> 4) & mask6);
-      arr[boff + 4] = static_cast<uint8_t>(poly[poff + 3].v >> 2);
+      const auto t0 = poly[poff + 0].to_canonical();
+      const auto t1 = poly[poff + 1].to_canonical();
+      const auto t2 = poly[poff + 2].to_canonical();
+      const auto t3 = poly[poff + 3].to_canonical();
+
+      arr[boff + 0] = static_cast<uint8_t>(t0);
+      arr[boff + 1] = static_cast<uint8_t>((t1 & mask6) << 2) |
+                      static_cast<uint8_t>((t0 >> 8) & mask2);
+      arr[boff + 2] = static_cast<uint8_t>((t2 & mask4) << 4) |
+                      static_cast<uint8_t>((t1 >> 6) & mask4);
+      arr[boff + 3] = static_cast<uint8_t>((t3 & mask2) << 6) |
+                      static_cast<uint8_t>((t2 >> 4) & mask6);
+      arr[boff + 4] = static_cast<uint8_t>(t3 >> 2);
     }
   } else if constexpr (l == 11) {
     constexpr size_t itr_cnt = ntt::N >> 3;
-    constexpr uint16_t mask8 = 0b11111111;
-    constexpr uint16_t mask7 = 0b1111111;
-    constexpr uint16_t mask6 = 0b111111;
-    constexpr uint16_t mask5 = 0b11111;
-    constexpr uint16_t mask4 = 0b1111;
-    constexpr uint16_t mask3 = 0b111;
-    constexpr uint16_t mask2 = 0b11;
-    constexpr uint16_t mask1 = 0b1;
+    constexpr uint32_t mask8 = 0b11111111u;
+    constexpr uint32_t mask7 = 0b1111111u;
+    constexpr uint32_t mask6 = 0b111111u;
+    constexpr uint32_t mask5 = 0b11111u;
+    constexpr uint32_t mask4 = 0b1111u;
+    constexpr uint32_t mask3 = 0b111u;
+    constexpr uint32_t mask2 = 0b11u;
+    constexpr uint32_t mask1 = 0b1u;
 
     for (size_t i = 0; i < itr_cnt; i++) {
       const size_t poff = i << 3;
       const size_t boff = i * 11;
 
-      arr[boff + 0] = static_cast<uint8_t>(poly[poff + 0].v & mask8);
-      arr[boff + 1] = static_cast<uint8_t>((poly[poff + 1].v & mask5) << 3) |
-                      static_cast<uint8_t>((poly[poff + 0].v >> 8) & mask3);
-      arr[boff + 2] = static_cast<uint8_t>((poly[poff + 2].v & mask2) << 6) |
-                      static_cast<uint8_t>((poly[poff + 1].v >> 5) & mask6);
-      arr[boff + 3] = static_cast<uint8_t>((poly[poff + 2].v >> 2) & mask8);
-      arr[boff + 4] = static_cast<uint8_t>((poly[poff + 3].v & mask7) << 1) |
-                      static_cast<uint8_t>((poly[poff + 2].v >> 10) & mask1);
-      arr[boff + 5] = static_cast<uint8_t>((poly[poff + 4].v & mask4) << 4) |
-                      static_cast<uint8_t>((poly[poff + 3].v >> 7) & mask4);
-      arr[boff + 6] = static_cast<uint8_t>((poly[poff + 5].v & mask1) << 7) |
-                      static_cast<uint8_t>((poly[poff + 4].v >> 4) & mask7);
-      arr[boff + 7] = static_cast<uint8_t>((poly[poff + 5].v >> 1) & mask8);
-      arr[boff + 8] = static_cast<uint8_t>((poly[poff + 6].v & mask6) << 2) |
-                      static_cast<uint8_t>((poly[poff + 5].v >> 9) & mask2);
-      arr[boff + 9] = static_cast<uint8_t>((poly[poff + 7].v & mask3) << 5) |
-                      static_cast<uint8_t>((poly[poff + 6].v >> 6) & mask5);
-      arr[boff + 10] = static_cast<uint8_t>((poly[poff + 7].v >> 3) & mask8);
+      const auto t0 = poly[poff + 0].to_canonical();
+      const auto t1 = poly[poff + 1].to_canonical();
+      const auto t2 = poly[poff + 2].to_canonical();
+      const auto t3 = poly[poff + 3].to_canonical();
+      const auto t4 = poly[poff + 4].to_canonical();
+      const auto t5 = poly[poff + 5].to_canonical();
+      const auto t6 = poly[poff + 6].to_canonical();
+      const auto t7 = poly[poff + 7].to_canonical();
+
+      arr[boff + 0] = static_cast<uint8_t>(t0 & mask8);
+      arr[boff + 1] = static_cast<uint8_t>((t1 & mask5) << 3) |
+                      static_cast<uint8_t>((t0 >> 8) & mask3);
+      arr[boff + 2] = static_cast<uint8_t>((t2 & mask2) << 6) |
+                      static_cast<uint8_t>((t1 >> 5) & mask6);
+      arr[boff + 3] = static_cast<uint8_t>((t2 >> 2) & mask8);
+      arr[boff + 4] = static_cast<uint8_t>((t3 & mask7) << 1) |
+                      static_cast<uint8_t>((t2 >> 10) & mask1);
+      arr[boff + 5] = static_cast<uint8_t>((t4 & mask4) << 4) |
+                      static_cast<uint8_t>((t3 >> 7) & mask4);
+      arr[boff + 6] = static_cast<uint8_t>((t5 & mask1) << 7) |
+                      static_cast<uint8_t>((t4 >> 4) & mask7);
+      arr[boff + 7] = static_cast<uint8_t>((t5 >> 1) & mask8);
+      arr[boff + 8] = static_cast<uint8_t>((t6 & mask6) << 2) |
+                      static_cast<uint8_t>((t5 >> 9) & mask2);
+      arr[boff + 9] = static_cast<uint8_t>((t7 & mask3) << 5) |
+                      static_cast<uint8_t>((t6 >> 6) & mask5);
+      arr[boff + 10] = static_cast<uint8_t>((t7 >> 3) & mask8);
     }
   } else {
     static_assert(l == 12, "l must be equal to 12 !");
 
     constexpr size_t itr_cnt = ntt::N >> 1;
-    constexpr uint16_t mask4 = 0b1111;
+    constexpr uint32_t mask4 = 0b1111u;
 
     for (size_t i = 0; i < itr_cnt; i++) {
       const size_t poff = i << 1;
       const size_t boff = i * 3;
 
-      arr[boff + 0] = static_cast<uint8_t>(poly[poff + 0].v);
-      arr[boff + 1] = static_cast<uint8_t>((poly[poff + 1].v & mask4) << 4) |
-                      static_cast<uint8_t>((poly[poff + 0].v >> 8) & mask4);
-      arr[boff + 2] = static_cast<uint8_t>(poly[poff + 1].v >> 4);
+      const auto t0 = poly[poff + 0].to_canonical();
+      const auto t1 = poly[poff + 1].to_canonical();
+
+      arr[boff + 0] = static_cast<uint8_t>(t0);
+      arr[boff + 1] = static_cast<uint8_t>((t1 & mask4) << 4) |
+                      static_cast<uint8_t>((t0 >> 8) & mask4);
+      arr[boff + 2] = static_cast<uint8_t>(t1 >> 4);
     }
   }
 }

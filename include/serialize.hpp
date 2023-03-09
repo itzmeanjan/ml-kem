8d29f90be0c85238057fea53a5cdc90bfb251947
Anjan Roy <hello@itzmeanjan.in>
2023-03-09 15:54:51 +0400

    update deserialization of bytearray to polynomial
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/serialize.hpp
+++ include/serialize.hpp
@@ -178,7 +178,7 @@ encode(const field::zq_t* const __restrict poly, // degree 255 polynomial
 template<const size_t l>
 static inline void
 decode(const uint8_t* const __restrict arr, // byte array of length 32*l -bytes
-       ff::ff_t* const __restrict poly      // degree 255 polynomial
+       field::zq_t* const __restrict poly   // degree 255 polynomial
        )
   requires(kyber_params::check_l(l))
 {
@@ -190,14 +190,14 @@ decode(const uint8_t* const __restrict arr, // byte array of length 32*l -bytes
       const size_t off = i << 3;
       const uint8_t byte = arr[i];
 
-      poly[off + 0].v = static_cast<uint16_t>((byte >> 0) & one);
-      poly[off + 1].v = static_cast<uint16_t>((byte >> 1) & one);
-      poly[off + 2].v = static_cast<uint16_t>((byte >> 2) & one);
-      poly[off + 3].v = static_cast<uint16_t>((byte >> 3) & one);
-      poly[off + 4].v = static_cast<uint16_t>((byte >> 4) & one);
-      poly[off + 5].v = static_cast<uint16_t>((byte >> 5) & one);
-      poly[off + 6].v = static_cast<uint16_t>((byte >> 6) & one);
-      poly[off + 7].v = static_cast<uint16_t>((byte >> 7) & one);
+      poly[off + 0] = field::zq_t::from_canonical((byte >> 0) & one);
+      poly[off + 1] = field::zq_t::from_canonical((byte >> 1) & one);
+      poly[off + 2] = field::zq_t::from_canonical((byte >> 2) & one);
+      poly[off + 3] = field::zq_t::from_canonical((byte >> 3) & one);
+      poly[off + 4] = field::zq_t::from_canonical((byte >> 4) & one);
+      poly[off + 5] = field::zq_t::from_canonical((byte >> 5) & one);
+      poly[off + 6] = field::zq_t::from_canonical((byte >> 6) & one);
+      poly[off + 7] = field::zq_t::from_canonical((byte >> 7) & one);
     }
   } else if constexpr (l == 4) {
     constexpr size_t itr_cnt = ntt::N >> 1;
@@ -207,8 +207,8 @@ decode(const uint8_t* const __restrict arr, // byte array of length 32*l -bytes
       const size_t off = i << 1;
       const uint8_t byte = arr[i];
 
-      poly[off + 0].v = static_cast<uint16_t>((byte >> 0) & mask);
-      poly[off + 1].v = static_cast<uint16_t>((byte >> 4) & mask);
+      poly[off + 0] = field::zq_t::from_canonical((byte >> 0) & mask);
+      poly[off + 1] = field::zq_t::from_canonical((byte >> 4) & mask);
     }
   } else if constexpr (l == 5) {
     constexpr size_t itr_cnt = ntt::N >> 3;
@@ -222,18 +222,27 @@ decode(const uint8_t* const __restrict arr, // byte array of length 32*l -bytes
       const size_t poff = i << 3;
       const size_t boff = i * 5;
 
-      poly[poff + 0].v = static_cast<uint16_t>(arr[boff + 0] & mask5);
-      poly[poff + 1].v = static_cast<uint16_t>((arr[boff + 1] & mask2) << 3) |
-                         static_cast<uint16_t>((arr[boff + 0] >> 5) & mask3);
-      poly[poff + 2].v = static_cast<uint16_t>((arr[boff + 1] >> 2) & mask5);
-      poly[poff + 3].v = static_cast<uint16_t>((arr[boff + 2] & mask4) << 1) |
-                         static_cast<uint16_t>((arr[boff + 1] >> 7) & mask1);
-      poly[poff + 4].v = static_cast<uint16_t>((arr[boff + 3] & mask1) << 4) |
-                         static_cast<uint16_t>((arr[boff + 2] >> 4) & mask4);
-      poly[poff + 5].v = static_cast<uint16_t>((arr[boff + 3] >> 1) & mask5);
-      poly[poff + 6].v = static_cast<uint16_t>((arr[boff + 4] & mask3) << 2) |
-                         static_cast<uint16_t>((arr[boff + 3] >> 6) & mask2);
-      poly[poff + 7].v = static_cast<uint16_t>((arr[boff + 4] >> 3) & mask5);
+      const auto t0 = static_cast<uint16_t>(arr[boff + 0] & mask5);
+      const auto t1 = static_cast<uint16_t>((arr[boff + 1] & mask2) << 3) |
+                      static_cast<uint16_t>((arr[boff + 0] >> 5) & mask3);
+      const auto t2 = static_cast<uint16_t>((arr[boff + 1] >> 2) & mask5);
+      const auto t3 = static_cast<uint16_t>((arr[boff + 2] & mask4) << 1) |
+                      static_cast<uint16_t>((arr[boff + 1] >> 7) & mask1);
+      const auto t4 = static_cast<uint16_t>((arr[boff + 3] & mask1) << 4) |
+                      static_cast<uint16_t>((arr[boff + 2] >> 4) & mask4);
+      const auto t5 = static_cast<uint16_t>((arr[boff + 3] >> 1) & mask5);
+      const auto t6 = static_cast<uint16_t>((arr[boff + 4] & mask3) << 2) |
+                      static_cast<uint16_t>((arr[boff + 3] >> 6) & mask2);
+      const auto t7 = static_cast<uint16_t>((arr[boff + 4] >> 3) & mask5);
+
+      poly[poff + 0] = field::zq_t::from_canonical(t0);
+      poly[poff + 1] = field::zq_t::from_canonical(t1);
+      poly[poff + 2] = field::zq_t::from_canonical(t2);
+      poly[poff + 3] = field::zq_t::from_canonical(t3);
+      poly[poff + 4] = field::zq_t::from_canonical(t4);
+      poly[poff + 5] = field::zq_t::from_canonical(t5);
+      poly[poff + 6] = field::zq_t::from_canonical(t6);
+      poly[poff + 7] = field::zq_t::from_canonical(t7);
     }
   } else if constexpr (l == 10) {
     constexpr size_t itr_cnt = ntt::N >> 2;
@@ -245,14 +254,19 @@ decode(const uint8_t* const __restrict arr, // byte array of length 32*l -bytes
       const size_t poff = i << 2;
       const size_t boff = i * 5;
 
-      poly[poff + 0].v = (static_cast<uint16_t>(arr[boff + 1] & mask2) << 8) |
-                         static_cast<uint16_t>(arr[boff + 0]);
-      poly[poff + 1].v = (static_cast<uint16_t>(arr[boff + 2] & mask4) << 6) |
-                         static_cast<uint16_t>(arr[boff + 1] >> 2);
-      poly[poff + 2].v = (static_cast<uint16_t>(arr[boff + 3] & mask6) << 4) |
-                         static_cast<uint16_t>(arr[boff + 2] >> 4);
-      poly[poff + 3].v = (static_cast<uint16_t>(arr[boff + 4]) << 2) |
-                         static_cast<uint16_t>(arr[boff + 3] >> 6);
+      const auto t0 = (static_cast<uint16_t>(arr[boff + 1] & mask2) << 8) |
+                      static_cast<uint16_t>(arr[boff + 0]);
+      const auto t1 = (static_cast<uint16_t>(arr[boff + 2] & mask4) << 6) |
+                      static_cast<uint16_t>(arr[boff + 1] >> 2);
+      const auto t2 = (static_cast<uint16_t>(arr[boff + 3] & mask6) << 4) |
+                      static_cast<uint16_t>(arr[boff + 2] >> 4);
+      const auto t3 = (static_cast<uint16_t>(arr[boff + 4]) << 2) |
+                      static_cast<uint16_t>(arr[boff + 3] >> 6);
+
+      poly[poff + 0] = field::zq_t::from_canonical(t0);
+      poly[poff + 1] = field::zq_t::from_canonical(t1);
+      poly[poff + 2] = field::zq_t::from_canonical(t2);
+      poly[poff + 3] = field::zq_t::from_canonical(t3);
     }
   } else if constexpr (l == 11) {
     constexpr size_t itr_cnt = ntt::N >> 3;
@@ -268,24 +282,33 @@ decode(const uint8_t* const __restrict arr, // byte array of length 32*l -bytes
       const size_t poff = i << 3;
       const size_t boff = i * 11;
 
-      poly[poff + 0].v = (static_cast<uint16_t>(arr[boff + 1] & mask3) << 8) |
-                         static_cast<uint16_t>(arr[boff + 0]);
-      poly[poff + 1].v = (static_cast<uint16_t>(arr[boff + 2] & mask6) << 5) |
-                         static_cast<uint16_t>(arr[boff + 1] >> 3);
-      poly[poff + 2].v = (static_cast<uint16_t>(arr[boff + 4] & mask1) << 10) |
-                         (static_cast<uint16_t>(arr[boff + 3]) << 2) |
-                         static_cast<uint16_t>(arr[boff + 2] >> 6);
-      poly[poff + 3].v = (static_cast<uint16_t>(arr[boff + 5] & mask4) << 7) |
-                         static_cast<uint16_t>(arr[boff + 4] >> 1);
-      poly[poff + 4].v = (static_cast<uint16_t>(arr[boff + 6] & mask7) << 4) |
-                         static_cast<uint16_t>(arr[boff + 5] >> 4);
-      poly[poff + 5].v = (static_cast<uint16_t>(arr[boff + 8] & mask2) << 9) |
-                         (static_cast<uint16_t>(arr[boff + 7]) << 1) |
-                         static_cast<uint16_t>(arr[boff + 6] >> 7);
-      poly[poff + 6].v = (static_cast<uint16_t>(arr[boff + 9] & mask5) << 6) |
-                         static_cast<uint16_t>(arr[boff + 8] >> 2);
-      poly[poff + 7].v = (static_cast<uint16_t>(arr[boff + 10]) << 3) |
-                         static_cast<uint16_t>(arr[boff + 9] >> 5);
+      const auto t0 = (static_cast<uint16_t>(arr[boff + 1] & mask3) << 8) |
+                      static_cast<uint16_t>(arr[boff + 0]);
+      const auto t1 = (static_cast<uint16_t>(arr[boff + 2] & mask6) << 5) |
+                      static_cast<uint16_t>(arr[boff + 1] >> 3);
+      const auto t2 = (static_cast<uint16_t>(arr[boff + 4] & mask1) << 10) |
+                      (static_cast<uint16_t>(arr[boff + 3]) << 2) |
+                      static_cast<uint16_t>(arr[boff + 2] >> 6);
+      const auto t3 = (static_cast<uint16_t>(arr[boff + 5] & mask4) << 7) |
+                      static_cast<uint16_t>(arr[boff + 4] >> 1);
+      const auto t4 = (static_cast<uint16_t>(arr[boff + 6] & mask7) << 4) |
+                      static_cast<uint16_t>(arr[boff + 5] >> 4);
+      const auto t5 = (static_cast<uint16_t>(arr[boff + 8] & mask2) << 9) |
+                      (static_cast<uint16_t>(arr[boff + 7]) << 1) |
+                      static_cast<uint16_t>(arr[boff + 6] >> 7);
+      const auto t6 = (static_cast<uint16_t>(arr[boff + 9] & mask5) << 6) |
+                      static_cast<uint16_t>(arr[boff + 8] >> 2);
+      const auto t7 = (static_cast<uint16_t>(arr[boff + 10]) << 3) |
+                      static_cast<uint16_t>(arr[boff + 9] >> 5);
+
+      poly[poff + 0] = field::zq_t::from_canonical(t0);
+      poly[poff + 1] = field::zq_t::from_canonical(t1);
+      poly[poff + 2] = field::zq_t::from_canonical(t2);
+      poly[poff + 3] = field::zq_t::from_canonical(t3);
+      poly[poff + 4] = field::zq_t::from_canonical(t4);
+      poly[poff + 5] = field::zq_t::from_canonical(t5);
+      poly[poff + 6] = field::zq_t::from_canonical(t6);
+      poly[poff + 7] = field::zq_t::from_canonical(t7);
     }
   } else {
     static_assert(l == 12, "l must be equal to 12 !");
@@ -297,10 +320,13 @@ decode(const uint8_t* const __restrict arr, // byte array of length 32*l -bytes
       const size_t poff = i << 1;
       const size_t boff = i * 3;
 
-      poly[poff + 0].v = (static_cast<uint16_t>(arr[boff + 1] & mask4) << 8) |
-                         static_cast<uint16_t>(arr[boff + 0]);
-      poly[poff + 1].v = (static_cast<uint16_t>(arr[boff + 2]) << 4) |
-                         static_cast<uint16_t>(arr[boff + 1] >> 4);
+      const auto t0 = (static_cast<uint16_t>(arr[boff + 1] & mask4) << 8) |
+                      static_cast<uint16_t>(arr[boff + 0]);
+      const auto t1 = (static_cast<uint16_t>(arr[boff + 2]) << 4) |
+                      static_cast<uint16_t>(arr[boff + 1] >> 4);
+
+      poly[poff + 0] = field::zq_t::from_canonical(t0);
+      poly[poff + 1] = field::zq_t::from_canonical(t1);
     }
   }
 }

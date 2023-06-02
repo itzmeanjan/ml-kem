2892144772652ebf16e48f77e69d58e5432a48f7
Anjan Roy <hello@itzmeanjan.in>
2023-06-02 18:22:51 +0400

    update kyber KEM benchmark routines, remove kyber PKE benchmark helper routines
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -1,4 +1,4 @@
-#include "bench/bench_kyber.hpp"
+#include "bench/bench_kem.hpp"
 
 auto compute_min = [](const std::vector<double>& v) -> double {
   return *std::min_element(v.begin(), v.end());
@@ -8,91 +8,47 @@ auto compute_max = [](const std::vector<double>& v) -> double {
   return *std::max_element(v.begin(), v.end());
 };
 
-// Register for benchmarking IND-CPA-secure Kyber Public Key Encryption
-
-// Kyber512
-BENCHMARK(bench_kyber::pke_keygen<2, 3>)
-  ->Name("kyber512_pke_keygen")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_kyber::encrypt<2, 3, 2, 10, 4>)
-  ->Name("kyber512_pke_encrypt")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_kyber::decrypt<2, 3, 2, 10, 4>)
-  ->Name("kyber512_pke_decrypt")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-
-// Kyber768
-BENCHMARK(bench_kyber::pke_keygen<3, 2>)
-  ->Name("kyber768_pke_keygen")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_kyber::encrypt<3, 2, 2, 10, 4>)
-  ->Name("kyber768_pke_encrypt")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_kyber::decrypt<3, 2, 2, 10, 4>)
-  ->Name("kyber768_pke_decrypt")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-
-// Kyber1024
-BENCHMARK(bench_kyber::pke_keygen<4, 2>)
-  ->Name("kyber1024_pke_keygen")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_kyber::encrypt<4, 2, 2, 11, 5>)
-  ->Name("kyber1024_pke_encrypt")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-BENCHMARK(bench_kyber::decrypt<4, 2, 2, 11, 5>)
-  ->Name("kyber1024_pke_decrypt")
-  ->ComputeStatistics("min", compute_min)
-  ->ComputeStatistics("max", compute_max);
-
 // Register for benchmarking IND-CCA2-secure Kyber Key Encapsulation Mechanism
 
 // Kyber512
-BENCHMARK(bench_kyber::kem_keygen<2, 3>)
-  ->Name("kyber512_kem_keygen")
+BENCHMARK(bench_kyber::keygen<2, 3>)
+  ->Name("kyber512_keygen")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
 BENCHMARK(bench_kyber::encapsulate<2, 3, 2, 10, 4>)
-  ->Name("kyber512_kem_encap")
+  ->Name("kyber512_encap")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
 BENCHMARK(bench_kyber::decapsulate<2, 3, 2, 10, 4>)
-  ->Name("kyber512_kem_decap")
+  ->Name("kyber512_decap")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
 
 // Kyber768
-BENCHMARK(bench_kyber::kem_keygen<3, 2>)
-  ->Name("kyber768_kem_keygen")
+BENCHMARK(bench_kyber::keygen<3, 2>)
+  ->Name("kyber768_keygen")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
 BENCHMARK(bench_kyber::encapsulate<3, 2, 2, 10, 4>)
-  ->Name("kyber768_kem_encap")
+  ->Name("kyber768_encap")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
 BENCHMARK(bench_kyber::decapsulate<3, 2, 2, 10, 4>)
-  ->Name("kyber768_kem_decap")
+  ->Name("kyber768_decap")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
 
 // Kyber1024
-BENCHMARK(bench_kyber::kem_keygen<4, 2>)
-  ->Name("kyber1024_kem_keygen")
+BENCHMARK(bench_kyber::keygen<4, 2>)
+  ->Name("kyber1024_keygen")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
 BENCHMARK(bench_kyber::encapsulate<4, 2, 2, 11, 5>)
-  ->Name("kyber1024_kem_encap")
+  ->Name("kyber1024_encap")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
 BENCHMARK(bench_kyber::decapsulate<4, 2, 2, 11, 5>)
-  ->Name("kyber1024_kem_decap")
+  ->Name("kyber1024_decap")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
 

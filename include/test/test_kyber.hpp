685357ec607824bb6243a548b0a46b8efd68732c
Anjan Roy <hello@itzmeanjan.in>
2023-06-02 19:10:32 +0400

    perform correctness and conformance test of Kyber512 KEM using C++ API and KATs
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/test/test_kyber.hpp
+++ include/test/test_kyber.hpp
@@ -3,5 +3,6 @@
 #include "test_compression.hpp"
 #include "test_field.hpp"
 #include "test_kem.hpp"
+#include "test_kem_kat.hpp"
 #include "test_ntt.hpp"
 #include "test_serialize.hpp"
--- test/main.cpp
+++ test/main.cpp
@@ -5,10 +5,10 @@ int
 main()
 {
   test_kyber::test_field_ops();
-  std::cout << "[test] Kyber prime field operations" << std::endl;
+  std::cout << "[test] Kyber prime field operations\n";
 
   test_kyber::test_ntt_intt();
-  std::cout << "[test] (i)NTT over degree-255 polynomial" << std::endl;
+  std::cout << "[test] (i)NTT over degree-255 polynomial\n";
 
   test_kyber::test_serialization<12>();
   test_kyber::test_serialization<11>();
@@ -16,14 +16,14 @@ main()
   test_kyber::test_serialization<5>();
   test_kyber::test_serialization<4>();
   test_kyber::test_serialization<1>();
-  std::cout << "[test] Polynomial serialization/ deserialization" << std::endl;
+  std::cout << "[test] Polynomial serialization/ deserialization\n";
 
   test_kyber::test_compression<11>();
   test_kyber::test_compression<10>();
   test_kyber::test_compression<5>();
   test_kyber::test_compression<4>();
   test_kyber::test_compression<1>();
-  std::cout << "[test] Coefficient compression/ decompression" << std::endl;
+  std::cout << "[test] Coefficient compression/ decompression\n";
 
   test_kyber::test_kyber_kem<2, 3, 2, 10, 4, 32>(); // kyber-512, 32B -key
   test_kyber::test_kyber_kem<3, 2, 2, 10, 4, 32>(); // kyber-768, 32B -key
@@ -34,7 +34,10 @@ main()
   test_kyber::test_kyber_kem<2, 3, 2, 10, 4, 64>(); // kyber-512, 64B -key
   test_kyber::test_kyber_kem<3, 2, 2, 10, 4, 64>(); // kyber-768, 64B -key
   test_kyber::test_kyber_kem<4, 2, 2, 11, 5, 64>(); // kyber-1024, 64B -key
-  std::cout << "[test] INDCCA2-secure Key Encapsulation Mechanism" << std::endl;
+  std::cout << "[test] INDCCA2-secure Kyber KEM\n";
+
+  test_kyber::test_kyber512_kem_kat();
+  std::cout << "[test] Kyber KEM Known Answer Tests\n";
 
   return EXIT_SUCCESS;
 }

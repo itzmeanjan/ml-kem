5589c97b6999884b65124b7bc4fa0b2014d089e4
Anjan Roy <hello@itzmeanjan.in>
2023-06-02 18:43:32 +0400

    given hex string, parse it as byte array
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "params.hpp"
+#include <array>
+#include <cassert>
+#include <charconv>
 #include <cstddef>
 #include <cstdint>
 #include <iomanip>
@@ -21,6 +24,32 @@ to_hex(const uint8_t* const bytes, const size_t len)
   }
 
   return ss.str();
+}
+
+// Given a hex encoded string of length 2*L, this routine can be used for
+// parsing it as a byte array of length L.
+template<const size_t L>
+inline std::array<uint8_t, L>
+from_hex(std::string_view bytes)
+{
+  const size_t blen = bytes.length();
+
+  assert(blen % 2 == 0);
+  assert(blen / 2 == L);
+
+  std::array<uint8_t, L> res{};
+
+  for (size_t i = 0; i < L; i++) {
+    const size_t off = i * 2;
+
+    uint8_t byte = 0;
+    auto sstr = bytes.substr(off, 2);
+    std::from_chars(sstr.data(), sstr.data() + 2, byte, 16);
+
+    res[i] = byte;
+  }
+
+  return res;
 }
 
 // Compile-time compute IND-CCA-secure Kyber KEM public key length ( in bytes )

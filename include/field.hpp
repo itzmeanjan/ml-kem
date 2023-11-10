85b0a3cee102c03222fcb258d818459a8dedd063
Anjan Roy <hello@itzmeanjan.in>
2023-11-10 16:28:17 +0530

    switch to using barrett reduction instead of using montgomery form and reduction
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/field.hpp
+++ include/field.hpp
@@ -4,10 +4,6 @@
 #include <cstdint>
 
 // Prime field arithmetic over Zq, for Kyber PQC Algorithm s.t. q = 3329
-//
-// For Montgomery Arithmetic it collects motivation from https://ia.cr/2017/1057
-// and
-// https://github.com/itzmeanjan/secp256k1/blob/37b339db/field/base_field.py
 namespace field {
 
 // Kyber Prime Field Modulus ( = 3329 )
@@ -16,120 +12,66 @@ constexpr uint32_t Q = (1u << 8) * 13 + 1;
 // Bit width of Kyber Prime Field Modulus ( = 12 )
 constexpr size_t RADIX_BIT_WIDTH = std::bit_width(Q);
 
-// = 2 ** RADIX_BIT_WIDTH = 2 ** 12 = 4096
-constexpr uint32_t RADIX = 1u << RADIX_BIT_WIDTH;
-
-// = 0x0FFF = 4095
-constexpr uint32_t MASK = RADIX - 1u;
-
-// Montgomery form of prime field element 1 ( = 767 )
-constexpr uint32_t R = RADIX % Q;
-
-// Required for converting an integer to its Montgomery Form ( = 2385 )
-constexpr uint32_t R2 = (R * R) % Q;
-
-// Compile-time computable constant μ, required for division less Montgomery
-// Multiplication ( with modulo reduction ), follows algo 3 of ia.cr/2017/1057
-constexpr uint32_t
-compute_mu()
-{
-  uint32_t y = 1u;
-
-  for (size_t i = 2; i < RADIX_BIT_WIDTH + 1; i++) {
-    const uint32_t t0 = Q * y;
-    const uint32_t t1 = t0 & ((1u << i) - 1u);
-    if (t1 != 1u) {
-      y = y + (1u << (i - 1));
-    }
-  }
-
-  return RADIX - y;
-}
-
-// Montgomery Constant μ ( = 3327 )
-constexpr uint32_t MU = compute_mu();
-
-// Prime field Zq | q = 3329, with arithmetic operations defined over it
+// Precomputed Barrett Reduction Constant
 //
-// Note, this implementation makes use of Montgomery technique for modulo
-// arithmetic.
+// Note,
+//
+// k = ceil(log2(Q)) = 12,
+// r = floor((1 << 2k) / Q) = 5039
+//
+// See https://www.nayuki.io/page/barrett-reduction-algorithm.
+constexpr uint32_t R = (1u << (2 * RADIX_BIT_WIDTH)) / Q;
+
+// Prime field Zq | q = 3329, with arithmetic operations defined over it.
 struct zq_t
 {
 public:
-  // Given an integer in Montgomery form, constructs a Zq object.
-  //
-  // Note, argument value must be <= RADIX !
-  //
-  // If you want to get a Zq object from an integer in canonical form, consider
-  // using `from_canonical()` routine.
-  inline constexpr zq_t(const uint32_t a = 0u) { v = a; }
-
-  // Given an integer in canonical form, converts it to Montgormery form.
-  static inline constexpr zq_t from_canonical(const uint16_t a)
+  // Given a 16 -bit unsigned integer `a`, this function constructs a Zq
+  // element, such that `a` is reduced modulo Q.
+  inline constexpr zq_t(const uint16_t a = 0u)
   {
-    return zq_t(mont_mul(a, R2));
+    this->v = barrett_reduce(static_cast<uint32_t>(a));
   }
 
-  // Given an integer in Montgomery Form, converts it to canonical form.
-  inline constexpr uint32_t to_canonical() const
+  inline constexpr uint32_t raw() const { return this->v; }
+
+  // Returns prime field element 1.
+  static inline constexpr zq_t one() { return zq_t(1u); }
+
+  // Returns prime field element 0.
+  static inline constexpr zq_t zero() { return zq_t(); }
+
+  // Modulo addition of two Zq elements.
+  inline constexpr zq_t operator+(const zq_t rhs) const
   {
-    const uint32_t t0 = mont_mul(v, 1u);
-    const uint32_t mask = -static_cast<uint32_t>(t0 >= Q);
-    return t0 - (Q & mask);
+    return zq_t(this->v + rhs.v);
   }
 
-  // Makes underlying value ( in Montgomery Form ) available.
-  inline constexpr uint32_t to_montgomery() const { return v; }
+  // Compound modulo addition of two Zq elements.
+  inline constexpr void operator+=(const zq_t rhs) { *this = *this + rhs; }
 
-  // Returns 1 as zq_t ( in Montgomery Form )
-  static inline constexpr zq_t one() { return zq_t::from_canonical(1u); }
+  // Modulo negation of a Zq element.
+  inline constexpr zq_t operator-() const { return zq_t(Q - this->v); }
 
-  // Modulo addition of two Zq elements ( in Montgomery Form )
-  inline constexpr zq_t operator+(const zq_t& rhs) const
-  {
-    constexpr uint32_t ONE = R; // = field::zq_t::one().to_montgomery()
-
-    uint32_t r = v + rhs.v;
-
-    r = (r >> RADIX_BIT_WIDTH) * ONE + (r & MASK);
-    r = (r >> RADIX_BIT_WIDTH) * ONE + (r & MASK);
-
-    return zq_t(r);
-  }
-
-  // Compound modulo addition of two Zq elements ( in Montgomery Form )
-  inline constexpr void operator+=(const zq_t& rhs) { *this = *this + rhs; }
-
-  // Modulo negation of Zq element ( in Montgomery Form )
-  inline constexpr zq_t operator-() const
-  {
-    constexpr uint32_t ONE = R; // = field::zq_t::one().to_montgomery()
-
-    uint32_t r = Q - v;
-    r = (r & MASK) - ((r >> RADIX_BIT_WIDTH) & ONE);
-
-    return zq_t(r);
-  }
-
-  // Modulo subtraction of two Zq elements ( in Montgomery Form )
-  inline constexpr zq_t operator-(const zq_t& rhs) const
+  // Modulo subtraction of one Zq element from another one.
+  inline constexpr zq_t operator-(const zq_t rhs) const
   {
     return *this + (-rhs);
   }
 
-  // Compound modulo subtraction of two Zq elements ( in Montgomery Form )
-  inline constexpr void operator-=(const zq_t& rhs) { *this = *this - rhs; }
+  // Compound modulo subtraction of two Zq elements.
+  inline constexpr void operator-=(const zq_t rhs) { *this = *this - rhs; }
 
-  // Modulo multiplication of two Zq elements ( in Montgomery Form )
-  inline constexpr zq_t operator*(const zq_t& rhs) const
+  // Modulo multiplication of two Zq elements.
+  inline constexpr zq_t operator*(const zq_t rhs) const
   {
-    return zq_t(mont_mul(v, rhs.v));
+    return zq_t(this->v * rhs.v);
   }
 
-  // Compound modulo multiplication of two Zq elements ( in Montgomery Form )
-  inline constexpr void operator*=(const zq_t& rhs) { *this = *this * rhs; }
+  // Compound modulo multiplication of two Zq elements.
+  inline constexpr void operator*=(const zq_t rhs) { *this = *this * rhs; }
 
-  // Modulo exponentiation of Zq element ( in Montgomery Form )
+  // Modulo exponentiation of Zq element.
   inline constexpr zq_t operator^(const size_t exp) const
   {
     auto res = zq_t::one();
@@ -145,25 +87,29 @@ public:
     return res;
   }
 
-  // Multiplicative inverse of Zq element ( in Montgomery Form ). Note, if Zq
-  // element is 0, we can't compute multiplicative inverse, so 0 is returned.
+  // Multiplicative inverse of Zq element.
+  //
+  // Note, if Zq element is 0, we can't compute multiplicative inverse and 0 is
+  // returned.
   inline constexpr zq_t inv() const
   {
     return *this ^ static_cast<size_t>((Q - 2));
   }
 
-  // Modulo division of two Zq elements ( in Montgomery Form ). Note, if
-  // denominator is 0, returned result is 0 too, becaue we can't compute
-  // multiplicative inverse of it.
-  inline constexpr zq_t operator/(const zq_t& rhs) const
+  // Modulo division of two Zq elements.
+  //
+  // Note, if denominator is 0, returned result is 0 too, becaue we can't
+  // compute multiplicative inverse of 0.
+  inline constexpr zq_t operator/(const zq_t rhs) const
   {
     return *this * rhs.inv();
   }
 
-  // Compare two Zq elements, returning truth value in case they are same
-  inline constexpr bool operator==(const zq_t& rhs) const
+  // Compare two Zq elements, returning truth value, in case they are same,
+  // otherwise returns false value.
+  inline constexpr bool operator==(const zq_t rhs) const
   {
-    return this->to_canonical() == rhs.to_canonical();
+    return this->v == rhs.v;
   }
 
   // Samples a random Zq element, using pseudo random number generator.
@@ -171,37 +117,31 @@ public:
   {
     uint16_t res = 0;
     prng.read(std::span(reinterpret_cast<uint8_t*>(&res), sizeof(res)));
-    return zq_t::from_canonical(res);
+
+    return zq_t(res);
   }
 
 private:
-  // Underlying value held in this type
-  uint32_t v = 0;
+  // Underlying value held in this type.
+  //
+  // Note, v is always kept in its canonical form i.e. v ∈ [0, Q).
+  uint32_t v = 0u;
 
-  // Given two integers ( in Montgomery Form ) performs Montgomery
-  // multiplication ( with modulo reduction ).
-  //
-  // This routine can be used for converting an integer ( in canonical form ) to
-  // its Montgomery form and also for getting it back to canonical form.
-  //
-  // Ensure atleast one of a, b is < 2**12, while other operand must be < 2**16
-  // Returned value will be < 2**12
-  static inline constexpr uint32_t mont_mul(const uint32_t a, const uint32_t b)
+  // Given a 32 -bit unsigned integer `v` such that `v` ∈ [0, Q*Q), this routine
+  // can be invoked for reducing `v` modulo Q, using barrett reduction
+  // technique, following algorithm description @
+  // https://www.nayuki.io/page/barrett-reduction-algorithm.
+  static inline constexpr uint32_t barrett_reduce(const uint32_t v)
   {
-    constexpr uint32_t ONE = R; // = field::zq_t::one().to_montgomery()
-    const uint32_t c = a * b;
+    const uint64_t t0 = static_cast<uint64_t>(v) * static_cast<uint64_t>(R);
+    const uint32_t t1 = static_cast<uint32_t>(t0 >> (2 * RADIX_BIT_WIDTH));
+    const uint32_t t2 = t1 * Q;
+    const uint32_t t = v - t2;
 
-    const uint64_t t = static_cast<uint64_t>(MU) * static_cast<uint64_t>(c);
-    const uint32_t q = static_cast<uint32_t>(t & static_cast<uint64_t>(MASK));
+    const bool flg = t >= Q;
+    const uint32_t t_prime = t - flg * Q;
 
-    uint32_t d = (c + (q * Q)) >> 12;
-
-    d = (d >> RADIX_BIT_WIDTH) * ONE + (d & MASK);
-    d = (d >> RADIX_BIT_WIDTH) * ONE + (d & MASK);
-    d = (d >> RADIX_BIT_WIDTH) * ONE + (d & MASK);
-
-    // now d must be <= RADIX now !
-    return d;
+    return t_prime;
   }
 };
 

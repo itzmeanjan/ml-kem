53c0afa6449b6f2202c5366a9f08d9c30b38543a
Anjan Roy <hello@itzmeanjan.in>
2024-09-01 23:13:58 +0400

    Declare lambda functions as `constexpr`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benchmarks/bench_helper.hpp
+++ benchmarks/bench_helper.hpp
@@ -2,5 +2,5 @@
 #include <algorithm>
 #include <vector>
 
-const auto compute_min = [](const std::vector<double>& v) -> double { return *std::min_element(v.begin(), v.end()); };
-const auto compute_max = [](const std::vector<double>& v) -> double { return *std::max_element(v.begin(), v.end()); };
+constexpr auto compute_min = [](const std::vector<double>& v) -> double { return *std::min_element(v.begin(), v.end()); };
+constexpr auto compute_max = [](const std::vector<double>& v) -> double { return *std::max_element(v.begin(), v.end()); };

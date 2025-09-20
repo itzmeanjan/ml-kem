f190c377199de665bb4e6359040a6230ddc33d24
Anjan Roy <hello@itzmeanjan.in>
2025-09-20 21:20:09 +0530

    Add script for automating sync. and regeneration of ACVP kats for ml-kem keygen algo
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ kats/scripts/sync_acvp_kats.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ACVP_SERVER_REPO_PATH="ACVP-Server"
+if [ -d "$ACVP_SERVER_REPO_PATH" ]; then
+    echo "> $ACVP_SERVER_REPO_PATH repository exists, let's just fetch latest."
+
+    pushd $ACVP_SERVER_REPO_PATH
+    git checkout master
+    git fetch
+    git pull origin master
+    popd
+else
+    echo "> $ACVP_SERVER_REPO_PATH repository doesn't exist, let's clone it."
+    git clone https://github.com/usnistgov/ACVP-Server.git
+fi
+
+cat ./ACVP-Server/gen-val/json-files/ML-KEM-keyGen-FIPS203/internalProjection.json | python parse_ml_kem_keygen_acvp_kat.py
+
+echo "> Generated all NIST ACVP KATs."

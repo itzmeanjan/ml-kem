fafe317d6f3d346b6d50dcbabe6b217652061843
Anjan Roy <hello@itzmeanjan.in>
2025-09-22 21:58:57 +0530

    Add Python script to parse ML-KEM encapKeyCheck KATs
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ kats/scripts/parse_ml_kem_pubkeyCheck_acvp_kat.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import json
+import sys
+import typing
+
+ML_KEM_512_ACVP_KAT_FILE_NAME="ml_kem_512_pubkeyCheck.acvp.kat"
+ML_KEM_768_ACVP_KAT_FILE_NAME="ml_kem_768_pubkeyCheck.acvp.kat"
+ML_KEM_1024_ACVP_KAT_FILE_NAME="ml_kem_1024_pubkeyCheck.acvp.kat"
+
+def extract_and_write_ml_kem_pubkeyCheck_kats(test_group: dict[str, typing.Any], write_to_file: str):
+    assert test_group["testType"] == "VAL"
+
+    with open(write_to_file, "wt") as fd:
+        for test in test_group["tests"]:
+            fd.write(f'pk = {test["ek"]}\n')
+            fd.write(f'sk = {test["dk"]}\n')
+            fd.write(f'testPassed = {test["testPassed"]}\n')
+            fd.write(f'reason = {test["reason"]}\n')
+
+            fd.write('\n')
+        
+        fd.flush()
+
+
+def main():
+    json_as_str = ''
+    for line in sys.stdin:
+        json_as_str += line
+    
+    acvp_kats = json.loads(json_as_str)
+
+    ml_kem_512_param_set = acvp_kats["testGroups"][7]
+    ml_kem_768_param_set = acvp_kats["testGroups"][9]
+    ml_kem_1024_param_set = acvp_kats["testGroups"][11]
+
+    extract_and_write_ml_kem_pubkeyCheck_kats(ml_kem_512_param_set, ML_KEM_512_ACVP_KAT_FILE_NAME)
+    extract_and_write_ml_kem_pubkeyCheck_kats(ml_kem_768_param_set, ML_KEM_768_ACVP_KAT_FILE_NAME)
+    extract_and_write_ml_kem_pubkeyCheck_kats(ml_kem_1024_param_set, ML_KEM_1024_ACVP_KAT_FILE_NAME)
+
+
+if __name__=='__main__':
+    main()
